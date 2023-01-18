0bb3f801456878aa50033ad9dadf1607aea57d54
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-01-18 15:15:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let textbox = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+textbox.addEventListener("input", function() {
+  letterCount.innerHTML = textbox.value.length;
+});
